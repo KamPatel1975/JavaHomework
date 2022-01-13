9c34db0da844c51f6419022efa65a766ecf2e8eb
KamPatel1975 <97128198+KamPatel1975@users.noreply.github.com>
2022-01-13 13:56:49 +0000

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,13 @@
+public class PositiveNegative {
+    public static void main (String[]args){
+
+        int a = 16;
+
+        if (a>0){
+            System.out.println("We have a positive number");
+
+        }
+        else if (a<0);
+            System.out.println("We have a negative number");
+        }
+    }
